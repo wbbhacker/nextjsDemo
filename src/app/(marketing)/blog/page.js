21828a3b23a8362a618e4bc1ce5a4fcf9c47a678
wbbhacker <2404016117@qq.com>
2023-08-12 16:05:00 +0800

    s

--- src/app/(marketing)/blog/page.js
+++ src/app/(marketing)/blog/page.js
@@ -1,19 +1,19 @@
 'use client'
-import Plotly from 'plotly.js';
+// import Plotly from 'plotly.js';
 import { useEffect } from 'react';
 
 const Blog = () => {
 
-  const fetchData = async ()=>{
-    const res = await fetch('blog/api')
-    const data = await res.json()
-    console.log(data)
-    // Plotly.newPlot('chartContainer', data.data, data.layout);
-    Plotly.react('chartContainer', data);
-  }
+  // const fetchData = async ()=>{
+  //   const res = await fetch('blog/api')
+  //   const data = await res.json()
+  //   console.log(data)
+  //   // Plotly.newPlot('chartContainer', data.data, data.layout);
+  //   Plotly.react('chartContainer', data);
+  // }
 
   useEffect(()=>{
-    fetchData()
+    // fetchData()
   },[])
 
   return <div  style={{
