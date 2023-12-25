a38b06563bef3bbfcad8d4fb8d4893c7e98157b1
wbbhacker <2404016117@qq.com>
2023-12-25 23:36:10 +0800

    docs:add isuss

--- src/app/(workspace)/ThemeProvider.js
+++ src/app/(workspace)/ThemeProvider.js
@@ -1,5 +1,5 @@
 'use client';
-
+// fixbug:https://github.com/vercel/next.js/discussions/22388
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { useEffect, useState } from 'react';
 
